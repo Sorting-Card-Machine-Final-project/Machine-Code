9021ab42aed2ab695f5a498e0109d3ff52924b06
Yuval <yuval.halperin7@gmail.com>
2024-08-23 17:38:53 +0300

    UART

--- SortingCardMachine-BluePill/Core/Src/main_UART.c
+++ SortingCardMachine-BluePill/Core/Src/main_UART.c
@@ -37,6 +37,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define RX_BUFFER_SIZE 2
 
 /* USER CODE END PD */
 
@@ -52,7 +53,7 @@ UART_HandleTypeDef huart1;
 
 
 /* USER CODE BEGIN PV */
-
+uint8_t rxBuffer[RX_BUFFER_SIZE];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -63,12 +64,12 @@ static void MX_USART1_UART_Init(void);
 
 
 /* USER CODE BEGIN PFP */
-void Uart_Tx_Rx(void);
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+QueueHandle_t xQueueUART;
 /* USER CODE END 0 */
 
 /**
@@ -79,6 +80,7 @@ int main(void)
 {
 
   /* USER CODE BEGIN 1 */
+  xQueueUART = xQueueCreate( 2, sizeof(uint8_t) );
 
   /* USER CODE END 1 */
 
@@ -276,7 +278,7 @@ static void MX_USART1_UART_Init(void)
     Error_Handler();
   }
   /* USER CODE BEGIN USART1_Init 2 */
-
+  HAL_UART_Receive_IT(&huart1, rxBuffer, RX_BUFFER_SIZE);
   /* USER CODE END USART1_Init 2 */
 
 }
@@ -329,26 +331,44 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
+  if (huart->Instance == USART1){
+    //HAL_UART_Receive(huart, &rxBuffer, RX_BUFFER_SIZE, HAL_MAX_DELAY);  // The function HAL_UART_Receive_IT already fill rxBuffer
+    /*
+    Here we need to deside what we are looking for at the messeges
+    For example we have first 7 bits that reffer to the suit and number (4 bits of number - 13 options, 2 bits of suit - 4 options)
+    We need to add the cell to go (2 bits),
+    and acknowledge 
 
+    The place for the extracktion can be when the program pull it from the Queue
+    */
+
+   if(xQueueSendFromISR(xQueueUART, rxBuffer, NULL) == errQUEUE_FULL){
+    //Queue is full - need to send a messege about it.
+   }
+  }
+
+  HAL_UART_Receive_IT(huart, rxBuffer, RX_BUFFER_SIZE);
+}
 /* USER CODE END 4 */
 
-/* USER CODE BEGIN Header_StartDefaultTask */
-/**
-  * @brief  Function implementing the defaultTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
-/* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void const * argument)
-{
-  /* USER CODE BEGIN 5 */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END 5 */
-}
+// /* USER CODE BEGIN Header_StartDefaultTask */
+// /**
+//   * @brief  Function implementing the defaultTask thread.
+//   * @param  argument: Not used
+//   * @retval None
+//   */
+// /* USER CODE END Header_StartDefaultTask */
+// void StartDefaultTask(void const * argument)
+// {
+//   /* USER CODE BEGIN 5 */
+//   /* Infinite loop */
+//   for(;;)
+//   {
+//     osDelay(1);
+//   }
+//   /* USER CODE END 5 */
+// }
 
 /**
   * @brief  Period elapsed callback in non blocking mode
