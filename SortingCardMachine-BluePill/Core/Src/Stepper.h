f72014157366b959d751efdc8b6ef7cf778c0d16
Yuval <yuval.halperin7@gmail.com>
2024-08-24 17:02:08 +0300

    Stepper modes

---
+++ SortingCardMachine-BluePill/Core/Src/Stepper.h
@@ -0,0 +1,6 @@
+#define STEPS_PER_REVOLUTION 200 // Need to define
+#define STEP_DELAY_MS 10 // Need to define
+#define MOTOR_TASK_PRIORITY osPriorityNormal
+#define MOTOR_TASK_STACK_SIZE 128
+#define FORWARS 1
+#define BACKWARDS 0
