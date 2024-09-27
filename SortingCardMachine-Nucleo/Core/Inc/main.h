d998eda06fef8c27dc915e082d05c71f5839e387
Yuval <yuval.halperin7@gmail.com>
2024-09-27 22:23:18 +0300

    Switching to nucleo

---
+++ SortingCardMachine-Nucleo/Core/Inc/main.h
@@ -0,0 +1,105 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define B1_Pin GPIO_PIN_13
+#define B1_GPIO_Port GPIOC
+#define USART_TX_Pin GPIO_PIN_2
+#define USART_TX_GPIO_Port GPIOA
+#define USART_RX_Pin GPIO_PIN_3
+#define USART_RX_GPIO_Port GPIOA
+#define LD2_Pin GPIO_PIN_5
+#define LD2_GPIO_Port GPIOA
+#define PWM1_Pin GPIO_PIN_6
+#define PWM1_GPIO_Port GPIOA
+#define STEPPER__STEP_Pin GPIO_PIN_1
+#define STEPPER__STEP_GPIO_Port GPIOB
+#define STEPPER__DIR_Pin GPIO_PIN_2
+#define STEPPER__DIR_GPIO_Port GPIOB
+#define SWITCH1_Pin GPIO_PIN_13
+#define SWITCH1_GPIO_Port GPIOB
+#define SWITCH2_Pin GPIO_PIN_14
+#define SWITCH2_GPIO_Port GPIOB
+#define PI_UART_TX_Pin GPIO_PIN_9
+#define PI_UART_TX_GPIO_Port GPIOA
+#define PI_UART_RX_Pin GPIO_PIN_10
+#define PI_UART_RX_GPIO_Port GPIOA
+#define STEPPER2_DIR_Pin GPIO_PIN_11
+#define STEPPER2_DIR_GPIO_Port GPIOA
+#define STEPPER2_STEP_Pin GPIO_PIN_12
+#define STEPPER2_STEP_GPIO_Port GPIOA
+#define TMS_Pin GPIO_PIN_13
+#define TMS_GPIO_Port GPIOA
+#define TCK_Pin GPIO_PIN_14
+#define TCK_GPIO_Port GPIOA
+#define SWO_Pin GPIO_PIN_3
+#define SWO_GPIO_Port GPIOB
+#define PWM2_Pin GPIO_PIN_6
+#define PWM2_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
